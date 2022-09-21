411044f21158c8c42a4f17c89d9560c28da77a20
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-09-21 14:38:21 +0200

    Update product-list's component class

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { product } from 'src/app/models/product';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   productList: product[] = [];
+  sub: Subscription = new Subscription;
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(product => {
+    this.sub = this.productService.getProducts().subscribe(product => {
       this.productList = product;
     });
   }
 
+  ngOnDestroy(): void {
+    this.productList = [];
+    this.sub.unsubscribe();
+  }
 }
