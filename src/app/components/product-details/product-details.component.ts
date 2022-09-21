b176e198f580265e381c122855b305fd3fda314d
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-09-21 14:38:58 +0200

    Update product-details component class

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { product } from 'src/app/models/product';
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
   productList: product[] = [];
   id: number = 1;
   item: product = {
@@ -20,11 +21,12 @@ export class ProductDetailsComponent implements OnInit {
     description: ''
   };
   amount: string ='1';
+  sub: Subscription = new Subscription;
   
   constructor(private productService: ProductService, private router: Router, private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(product => {
+    this.sub = this.productService.getProducts().subscribe(product => {
       this.id = parseInt(this.router.url.split('/')[2]);
       this.productList = product;
       this.item = product.filter(p => p.id === this.id)[0];
@@ -41,4 +43,17 @@ export class ProductDetailsComponent implements OnInit {
     alert('Added to cart!');
   }
 
+  ngOnDestroy(): void {
+    this.productList = [];
+    this.id = 1;
+    this.item = {
+      id: 1,
+      name: '',
+      price: 1,
+      url: '',
+      description: ''
+    };
+    this.amount ='1';
+    this.sub.unsubscribe();
+  }
 }
